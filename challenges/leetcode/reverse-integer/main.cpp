bc77430f203e2fb524d3f7e2ccd81072077b12f6
Elisban Pacco Accha <paccoacchaelias@gmail.com>
2026-02-28 00:44:25 -0500

    resolv reverse-integer

--- challenges/leetcode/reverse-integer/main.cpp
+++ challenges/leetcode/reverse-integer/main.cpp
@@ -5,8 +5,29 @@
  */
 
 #include <iostream>
+using namespace std;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
+int reverse(int x){
+    int reversed = 0;
+
+    while (x != 0) {
+        int pop = x % 10;
+        x /= 10;
+        if (reversed > INT_MAX/10 || (reversed == INT_MAX / 10 && pop > 7)) return 0;
+        if (reversed < INT_MIN/10 || (reversed == INT_MIN / 10 && pop < -8)) return 0;
+        reversed = reversed * 10 + pop;
+    }
+    return reversed;
+}
 
 int main() {
-    std::cout << "reverse-integer" << std::endl;
+    int x = -120;
+
+    cout<<reverse(x)<<endl;
+
+    // cout << "Hello world !!! " << endl;
     return 0;
 }

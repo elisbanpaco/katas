ae50a948b9354da7a65554d8333ad9a2a73f324c
Elisban Pacco Accha <paccoacchaelias@gmail.com>
2026-03-02 08:25:47 -0500

    resolv 401-a atcoder

---
+++ challenges/atcoder/401-a/README.md
@@ -0,0 +1,15 @@
+# 401-a
+
+atcoder - C++
+Created with Code Forge.
+
+## Description
+
+Add description here.
+
+## Usage
+
+```bash
+g++ main.cpp -o main
+./main
+```
---
+++ challenges/atcoder/401-a/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * atcoder: 401-a
+ * Language: C++
+ * Created with Code Forge
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int S;
+    cin>> S;
+
+    if (S>= 200 && S<=299){
+        cout << "Success";
+    }else {
+        cout<<"Failure";
+    }
+    return 0;
+}

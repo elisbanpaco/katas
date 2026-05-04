a047721cc6bec4d3910d09ba9e53ccc0ff851ece
elisbanpaco <paccoacchaelias@gmail.com>
2026-05-04 00:05:34 -0500

    feat: add 100. Same Tree

---
+++ challenges/leetcode/100-same-tree/main.cpp
@@ -0,0 +1,53 @@
+/**
+ * leetcode: 100-same-tree
+ * Language: C++
+ * Created with Code Forge
+ */
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+    }
+};
+
+// MAIN PARA PRUEBAS
+int main()
+{
+    Solution sol;
+
+    TreeNode *p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+    p->left->left = new TreeNode(4);
+    p->left->right = new TreeNode(5);
+    p->right->left = new TreeNode(6);
+    p->right->right = new TreeNode(7);
+
+    TreeNode *q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+    q->left->left = new TreeNode(4);
+    q->left->right = new TreeNode(5);
+    q->right->left = new TreeNode(6);
+    q->right->right = new TreeNode(7);
+
+    bool result = sol.isSameTree(p, q);
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}

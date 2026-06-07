01e05a3437d8ab1fb678f159e4f7d11c9e1db3ca
elisbanpaco <paccoacchaelias@gmail.com>
2026-06-06 22:02:36 -0500

    feat: add 195. Tenth Line challenge

---
+++ challenges/leetcode/195-tenth-line/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * leetcode: 195-tenth-line
+ * Language: C++
+ * Created with Code Forge
+ */
+
+#include <iostream>
+using namespace std;
+// Read from the file file.txt and output the tenth line to stdout.
+
+// Respuesta : sed -n '10p' file.txt
+
+
+
+// MAIN PARA PRUEBAS
+int main() {
+    cout << "sed -n '10p' file.txte" << endl;
+    return 0;
+}

66131f8f55ecd917ea4d0b8a26bc562005eab0a7
elisbanpaco <paccoacchaelias@gmail.com>
2026-07-16 23:45:17 -0500

    chore: solution 7-reverse-integer in leetcode

---
+++ challenges/leetcode/7-reverse-integer/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * leetcode: 7-reverse-integer
+ * Language: C++
+ * Created with Code Forge
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+};
+
+// MAIN PARA PRUEBAS
+int main(){
+    Solution sol;
+    int x = 123;
+    int result = sol.reverse(x);
+    cout << "El resultado es: " << result << endl;
+    return 0;
+}
---
+++ challenges/leetcode/7-reverse-integer/test.cpp
@@ -0,0 +1,11 @@
+#include <cassert>
+#include <iostream>
+
+std::string 7ReverseInteger();
+#include "solution.cpp"
+
+int main() {
+    assert(7ReverseInteger() == "7-reverse-integer");
+    std::cout << "Test passed!" << std::endl;
+    return 0;
+}

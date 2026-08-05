15e0463d257484579b3f4b9fa02fb74da78586a5
elisbanpaco <paccoacchaelias@gmail.com>
2026-08-04 23:47:14 -0500

    feat: add 16. 3Sum Closest challenge leetcode

---
+++ challenges/leetcode/16-3sum-closest/README.md
@@ -0,0 +1,15 @@
+# 16-3sum-closest
+
+leetcode - C++
+Created with Code Forge.
+
+## Description
+
+Add description here.
+
+## Usage
+
+```bash
+g++ main.cpp -o main
+./main
+```
---
+++ challenges/leetcode/16-3sum-closest/solution.cpp
@@ -0,0 +1,54 @@
+/**
+ * leetcode: 16-3sum-closest
+ * Language: C++
+ * Created with Code Forge
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+        int result = nums[0] + nums[1] + nums[2];
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
+                }
+                if (abs(sum - target) < abs(result - target)) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// MAIN PARA PRUEBAS
+int main(){
+    Solution sol;
+
+    vector<int> nums = {-1,2,1,-4};
+    int target = 1;
+
+    int result = sol.threeSumClosest(nums, target);
+
+    return 0;
+}
